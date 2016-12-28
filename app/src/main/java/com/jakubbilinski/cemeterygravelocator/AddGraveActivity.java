a699bf48f2a2e9de187a356db0bd20b1288f35ea
DESKTOP-MN48R0A\bilek <bilek993@wp.pl>
2016-12-28 09:57:57 +0100

    Added map activity.

--- app/src/main/java/com/jakubbilinski/cemeterygravelocator/AddGraveActivity.java
+++ app/src/main/java/com/jakubbilinski/cemeterygravelocator/AddGraveActivity.java
@@ -1,13 +1,27 @@
 package com.jakubbilinski.cemeterygravelocator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AddGraveActivity extends AppCompatActivity {
+
+    Button buttonEditLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_grave);
+
+        buttonEditLocation = (Button) findViewById(R.id.buttonEditLocation);
+        buttonEditLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getBaseContext(), MapSelectActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
