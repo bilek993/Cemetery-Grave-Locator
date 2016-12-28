2402da7df26d46470bf8fb38205fd42692c1eed5
DESKTOP-MN48R0A\bilek <bilek993@wp.pl>
2016-12-28 09:40:25 +0100

    Added new Activity for adding new items and added maps library.

--- app/src/main/java/com/jakubbilinski/cemeterygravelocator/MainActivity.java
+++ app/src/main/java/com/jakubbilinski/cemeterygravelocator/MainActivity.java
@@ -1,13 +1,27 @@
 package com.jakubbilinski.cemeterygravelocator;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton floatingActionButtonAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        floatingActionButtonAdd = (FloatingActionButton) findViewById(R.id.floatingActionButtonAdd);
+        floatingActionButtonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getBaseContext(), AddGraveActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
