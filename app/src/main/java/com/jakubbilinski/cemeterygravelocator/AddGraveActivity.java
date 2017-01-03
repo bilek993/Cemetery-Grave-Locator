997a4139322f14a15f2920d061bd294cfb4dd242
DESKTOP-MN48R0A\bilek <bilek993@wp.pl>
2017-01-03 13:27:58 +0100

    Image can now be seleceted from library and is loaded by picasso.

--- app/src/main/java/com/jakubbilinski/cemeterygravelocator/AddGraveActivity.java
+++ app/src/main/java/com/jakubbilinski/cemeterygravelocator/AddGraveActivity.java
@@ -2,23 +2,29 @@ package com.jakubbilinski.cemeterygravelocator;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.io.InputStream;
 
 public class AddGraveActivity extends AppCompatActivity {
 
     private final int MAP_REQUEST_ID = 73;
+    private final int IMAGE_REQUEST_ID = 92;
 
     private double latitude = 0;
     private double longitude = 0;
 
     private TextView textViewLatitude;
     private TextView textViewLongitude;
-
-    Button buttonEditLocation;
+    private ImageView imageViewPhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,18 +33,27 @@ public class AddGraveActivity extends AppCompatActivity {
 
         textViewLatitude = (TextView) findViewById(R.id.textViewLatitude);
         textViewLongitude = (TextView) findViewById(R.id.textViewLongitude);
+        imageViewPhoto = (ImageView) findViewById(R.id.imageViewPhoto);
 
         setButtons();
         updateUI();
     }
 
     private void setButtons() {
-        buttonEditLocation = (Button) findViewById(R.id.buttonEditLocation);
+        Button buttonEditLocation = (Button) findViewById(R.id.buttonEditLocation);
         buttonEditLocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getBaseContext(), MapSelectActivity.class);
                 startActivityForResult(intent,MAP_REQUEST_ID);
+            }
+        });
+
+        Button buttonEditImage = (Button) findViewById(R.id.buttonEditPhoto);
+        buttonEditImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectImage();
             }
         });
     }
@@ -48,12 +63,24 @@ public class AddGraveActivity extends AppCompatActivity {
         textViewLongitude.setText(getString(R.string.longitude) + " " + longitude);
     }
 
+    private void selectImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, getString(R.string.select_img)), IMAGE_REQUEST_ID);
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == MAP_REQUEST_ID) {
-            if (resultCode == Activity.RESULT_OK) {
-                latitude = data.getDoubleExtra(Tags.MAP_LATITUDE,0);
-                longitude = data.getDoubleExtra(Tags.MAP_LONGITUDE,0);
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == MAP_REQUEST_ID) {
+                latitude = data.getDoubleExtra(Tags.MAP_LATITUDE, 0);
+                longitude = data.getDoubleExtra(Tags.MAP_LONGITUDE, 0);
+            } else if (requestCode == IMAGE_REQUEST_ID) {
+                if (data != null) {
+                    Uri imageUri = data.getData();
+                    Picasso.with(this).load(imageUri).fit().centerInside().into(imageViewPhoto);
+                }
             }
         }
 
