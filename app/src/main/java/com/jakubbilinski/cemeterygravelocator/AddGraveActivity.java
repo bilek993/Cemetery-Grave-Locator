479de827affe6ecee7d4f93938e01393ee7bd348
DESKTOP-MN48R0A\bilek <bilek993@wp.pl>
2017-01-04 11:05:10 +0100

    Changed "replace" image and changed "save" from normal button to menu item.

--- app/src/main/java/com/jakubbilinski/cemeterygravelocator/AddGraveActivity.java
+++ app/src/main/java/com/jakubbilinski/cemeterygravelocator/AddGraveActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -37,6 +39,21 @@ public class AddGraveActivity extends AppCompatActivity {
 
         setButtons();
         updateUI();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_add_grave,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_item_save_and_add) {
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private void setButtons() {
