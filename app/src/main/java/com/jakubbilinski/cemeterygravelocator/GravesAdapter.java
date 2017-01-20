81ff309626596df3ae4efacf7adc94f34afa55ab
DESKTOP-MN48R0A\bilek <bilek993@wp.pl>
2017-01-20 13:08:39 +0100

    Fixed imports.

--- app/src/main/java/com/jakubbilinski/cemeterygravelocator/GravesAdapter.java
+++ app/src/main/java/com/jakubbilinski/cemeterygravelocator/GravesAdapter.java
@@ -19,7 +19,6 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 /**
  * Created by bilek on 03.01.2017.
--- app/src/main/java/com/jakubbilinski/cemeterygravelocator/MainActivity.java
+++ app/src/main/java/com/jakubbilinski/cemeterygravelocator/MainActivity.java
@@ -9,8 +9,6 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
 import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
